475affe6166f9a2ea50db1e8c0b504bdc210fbf4
khantmawhein <kokhant228abc@gmail.com>
2020-07-23 08:54:49 +0630

    Updates

---
+++ precache-manifest.73994d513cb1524fca39bd60cda56b56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8231840bc645f032ddd87cb5388b7084",
+    "url": "/my-youtube-clone/index.html"
+  },
+  {
+    "revision": "108f87f01882b72f77c2",
+    "url": "/my-youtube-clone/static/js/2.a49d260c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-youtube-clone/static/js/2.a49d260c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55252c052aa0cf39ae26",
+    "url": "/my-youtube-clone/static/js/main.0fd8502c.chunk.js"
+  },
+  {
+    "revision": "0de42cbd694d7987dbd1",
+    "url": "/my-youtube-clone/static/js/runtime-main.adb50453.js"
+  }
+]);
